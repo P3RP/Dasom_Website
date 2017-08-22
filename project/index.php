ba08dcdd8b8aaf5f093c31b2e3923b9a4f613f6d
xezout108 <xezout108@naver.com>
2017-08-22 16:31:59 +0900

    Project Page Update

---
+++ project/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+
+	<!-- Title -->
+	<title>Dasom | Project</title>
+
+	<!-- Meta Data -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, height=device-height">
+
+	<!-- Import CSS -->
+	<link rel="stylesheet" type="text/css" href="./css/project_index.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+	<!-- JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+	<!-- Site Wrapper -->
+	<div class="site-wrapper">
+
+		<!-- Nav -->
+		<div class="nav">
+			<a href="">Main</a>
+			<a href="">Community</a>
+		</div>
+
+		<!-- Main Page -->
+		<div class="main">
+			
+			<!-- Title -->
+			<div class="title title-text">
+				<p>Title</p>
+			</div>
+
+			<!-- Login Section -->
+			<section class="login-section">
+
+				<div class="input-wrapper">
+					<input type="text" name="id" class="login-section-content" placeholder="Username">
+				</div>
+
+				<div class="input-wrapper">
+					<input type="text" name="password" class="login-section-content" placeholder="Password">
+				</div>
+
+				<div class="">
+					<a type="button" class="btn btn-primary" href="./projectList.php">LOGIN</a>
+				</div>
+			</section>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
---
+++ project/projectList.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+
+	<!-- Title -->
+	<title>Dasom | Project</title>
+
+	<!-- Meta Data -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, height=device-height">
+
+	<!-- Import CSS -->
+	<link rel="stylesheet" type="text/css" href="./css/project_projectList.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+	<!-- JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+	<script>
+		function openNav() {
+		    document.getElementById("mySidenav").style.width = "250px";
+		}
+
+		function closeNav() {
+		    document.getElementById("mySidenav").style.width = "0";
+		}
+	</script>
+
+
+</head>
+<body>
+
+	<!-- Site Wrapper -->
+	<div class="site-wrapper">
+		
+		<div id="mySidenav" class="sidenav">
+		  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		  <a href="#">About</a>
+		  <a href="#">Services</a>
+		  <a href="#">Clients</a>
+		  <a href="#">Contact</a>
+		</div>
+
+		<!-- Slide Menu -->
+		<div class="menu">
+			<span onclick="openNav()" class="haha">&#9776; open</span>
+		</div>
+
+		<!-- Main Page -->
+		<div class="main">
+
+			<!-- Project Board -->
+			<div class="project-board">
+				
+				<!-- Project Board Title -->
+				<div class="project-board-title">
+					<h1>Project</h1>
+				</div>
+
+				<!-- Project List -->
+				<ul class="project-list">
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+					<li>
+						<div class="project">
+							<div class="project-title">
+								Title
+							</div>
+
+							<div class="project-date">
+								Date
+							</div>
+						</div>
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
